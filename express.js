2154a821502a5a89d58edddd0212bb31c084e985
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-05-04 22:34:33 +0300

    .

--- express.js
+++ express.js
@@ -3,10 +3,8 @@ import express from "express"
 const app = express()
 
 app.get("/",(req,res)=>{
-    console.log("...res")
     res.send("Сервер вообщето работает")
 })
 app.listen(5000,function(){
-    const p = this.address().port
-    console.log(p)
+    console.log(`Сервер работает на 5000`)
 })
